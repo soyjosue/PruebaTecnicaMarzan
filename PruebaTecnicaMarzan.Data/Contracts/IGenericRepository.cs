72b8f3e62aa439b99afd8dac7d4d2f65cfd3b3a1
einoa <josueinoa02@gmail.com>
2022-10-11 19:44:36 -0400

    * Se agrego el proyecto Models * Se agrego el proyecto Data * Se comenzo con la creación de los models * Se comenzo con la creación de los repositorios

---
+++ PruebaTecnicaMarzan.Data/Contracts/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaMarzan.Data.Contracts
+{
+    public interface IGenericRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetByIDAsync(int id);
+        Task<bool> CreateAsync(T entity);
+        Task<bool> EditAsync(T entity);
+        Task<bool> RemoveAsync(T entity);
+    }
+}
---
+++ PruebaTecnicaMarzan.Data/PTMContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaMarzan.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaTecnicaMarzan.Data
+{
+    public class PTMContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=PTMDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
