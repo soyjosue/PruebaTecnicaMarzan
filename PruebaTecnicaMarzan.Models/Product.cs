ed4c5a6a7ac4e25cfbcfafc2c3ffb3c0455e2f83
einoa <josueinoa02@gmail.com>
2022-10-12 00:18:47 -0400

    * Generado scaffolding Producto * Se cambio el estilo de la view Index y Create de products

--- PruebaTecnicaMarzan.Models/Product.cs
+++ PruebaTecnicaMarzan.Models/Product.cs
@@ -10,10 +10,13 @@ namespace PruebaTecnicaMarzan.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
         [Required(ErrorMessage = "El precio es obligatorio.")]
+        [Display(Name = "Precio")]
         public float Price { get; set; }
         [Required(ErrorMessage = "La fecha de creación es obligatoria.")]
+        [Display(Name = "Fecha Creación")]
         public DateTime CreatedAt { get; set; }
     }
 }
