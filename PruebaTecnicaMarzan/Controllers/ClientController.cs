2edc33ad372fcac1892053116dcf939d77926fc7
einoa <josueinoa02@gmail.com>
2022-10-12 00:59:18 -0400

    * Se inicio con el controlador de Client

---
+++ PruebaTecnicaMarzan/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaTecnicaMarzan.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
