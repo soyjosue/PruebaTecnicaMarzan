99cebf3f6975e15a5629e301119e6a1de39fbaef
einoa <josueinoa02@gmail.com>
2022-10-11 20:34:27 -0400

    * Vista inicio de sesión terminada.

---
+++ PruebaTecnicaMarzan.Dtos/SignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnicaMarzan.ViewModels
+{
+    public class SignInViewModel
+    {
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ PruebaTecnicaMarzan/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaTecnicaMarzan.ViewModels;
+
+namespace PruebaTecnicaMarzan.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(SignInViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            return View(model);
+        }
+    }
+}
